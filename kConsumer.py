aafcc22f24f64b73ed70f4d1eecbbbd6f6263d0d
Scott Chapin <bschapin@gmail.com>
2022-01-02 14:21:03 -0700

    initial commit

---
+++ kConsumer.py
@@ -0,0 +1,85 @@
+from confluent_kafka import Consumer, KafkaError, KafkaException
+import socket
+import argparse
+import sys
+
+running = True
+
+
+def msg_process(msg):
+    print(f"Key: {msg.topic()}{msg.partition()}{msg.offset()}, Message: {msg.value()}")
+
+
+def basic_consume_loop(consumer, topics):
+    try:
+        consumer.subscribe(topics)
+
+        while running:
+            msg = consumer.poll(timeout=1.0)
+            if msg is None:
+                continue
+
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    # End of partition event
+                    sys.stderr.write(
+                        "%% %s [%d] reached end at offset %d\n"
+                        % (msg.topic(), msg.partition(), msg.offset())
+                    )
+                elif msg.error():
+                    raise KafkaException(msg.error())
+            else:
+                msg_process(msg)
+    except KeyboardInterrupt:
+        print("Keyboard Ctrl-C interrupt")
+    finally:
+        # Close down consumer to commit final offsets.
+        print("Closing connection")
+        consumer.close()
+
+
+def shutdown():
+    running = False
+
+
+def main():
+
+    # Cluster Version
+    # conf = {
+    #     "bootstrap.servers": "localhost:29092,localhost:39092",
+    #     "group.id": args.group,
+    #     "auto.offset.reset": "smallest",
+    # }
+
+    # Single Version
+    conf = {
+        "bootstrap.servers": "localhost:29092",
+        "group.id": args.group,
+        "auto.offset.reset": "smallest",
+    }
+
+    consumer = Consumer(conf)
+
+    basic_consume_loop(consumer, args.topics)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Consume records from Kafka")
+
+    parser.add_argument(
+        "group",
+        help="consumer group",
+    )
+    parser.add_argument(
+        "--topics",
+        help="message topic",
+        required=True,
+        action="extend",
+        nargs="+",
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    main()
---
+++ kProducer.py
@@ -0,0 +1,76 @@
+from confluent_kafka import Producer, KafkaError, KafkaException
+import socket
+import argparse
+import json
+
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
+        print(
+            f"Latency: {msg.latency()}, Key: {msg.key()}, Topic: {msg.topic()}, Partition: {msg.partition()}, Offset: {msg.offset()}, Timestamp: {msg.timestamp()}"
+        )
+
+
+def main():
+
+    # Cluster Version
+    # conf = {
+    #     "bootstrap.servers": "localhost:29092,localhost:39092",
+    #     "client.id": socket.gethostname(),
+    # }
+
+    # Single Version
+    conf = {
+        "bootstrap.servers": "localhost:29092",
+        "client.id": socket.gethostname(),
+    }
+
+    producer = Producer(conf)
+
+    for i in range(args.iters):
+        record_dict = {}
+        key = str(i)
+        message = f"{args.message} {i}"
+        record_dict["payload"] = message
+        producer.produce(
+            args.topic,
+            key=key,
+            value=json.dumps(record_dict).encode("utf-8"),
+            callback=acked,
+        )
+
+        producer.poll(1)
+
+    print("Flushing records...")
+    producer.flush()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Produce records for Kafka")
+
+    parser.add_argument(
+        "--iters",
+        help="number of iterations",
+        required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "--topic",
+        help="message topic",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "--message",
+        help="message base",
+        required=True,
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    main()
